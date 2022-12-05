838ae489b15ce717c4a38ab311808bc12f0c4413
wangxin <2677855779@qq.com>
2022-12-05 23:12:27 +0800

    第一次提交

---
+++ components/newsItem/newsItem.vue
@@ -0,0 +1,87 @@
+<template>
+	<view class="newsItem">
+		<view class="pic">
+			<image :src="item.picurl" mode="aspectFill"></image>
+		</view>
+		<view class="text">
+			<view class="title">
+				{{item.title}}
+			</view>
+			<view class="info">
+				<text>{{item.author}}</text>
+				<text>{{item.hits}}浏览</text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "newsItem",
+		props: {
+			item: {
+				type: Object,
+				default: () => {
+					return {
+						title: "组件内部默认的标题",
+						author: "王鑫",
+						hits: 999,
+						picurl: "../../static/images/nopic.jpg",
+					}
+				}
+			}
+		},
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.newsItem {
+		display: flex;
+
+		.pic {
+			width: 230rpx;
+			height: 160rpx;
+
+			image {
+				width: 100%;
+				height: 100%;
+			}
+		}
+
+		.text {
+			flex: 1;
+			padding-left: 20rpx;
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+
+			.title {
+				font-size: 36rpx;
+				color: #333;
+				word-break: break-all;
+				word-wrap: break-word;
+				text-overflow: -o-ellipsis-lastline;
+				overflow: hidden; //溢出内容隐藏
+				text-overflow: ellipsis; //文本溢出部分用省略号表示
+				display: -webkit-box; //特别显示模式
+				-webkit-line-clamp: 2; //行数
+				line-clamp: 2;
+				-webkit-box-orient: vertical; //盒子中内容竖直排列	
+			}
+
+			.info {
+				font-size: 26rpx;
+				color: #999;
+
+				text {
+					padding-right: 30rpx;
+				}
+			}
+		}
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,75 @@
+<template>
+	<view class="home">
+		<scroll-view scroll-x="true" class="nav-scroll">
+			<view v-for="(item,index) in 10" :key="index" class="item" :class="{active:activeIndex === index}"
+				@click="handleClickNav(index)">国内</view>
+		</scroll-view>
+
+		<view class="content">
+			<view class="row" v-for="(item,index) in 20" :key="index">
+				<newsItem></newsItem>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				activeIndex: 0,
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			handleClickNav(index) {
+				console.log(index);
+				this.activeIndex = index;
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.home {
+		.nav-scroll {
+			height: 100rpx;
+			white-space: nowrap;
+			background: #F7F8FA;
+
+			.item {
+				font-size: 40rpx;
+				display: inline-block;
+				line-height: 100rpx;
+				padding: 0 30rpx;
+				color: #333;
+
+				&.active {
+					color: #31c27c;
+				}
+			}
+
+
+			// 隐藏 h5 端滚动条
+			/deep/ ::-webkit-scrollbar {
+				width: 4px !important;
+				height: 1px !important;
+				overflow: auto !important;
+				background: transparent !important;
+				-webkit-appearance: auto !important;
+				display: block;
+			}
+		}
+
+		.content {
+			padding: 30rpx;
+
+			.row {
+				border-bottom: 1px dotted #efefef;
+				padding: 20rpx 0;
+			}
+		}
+	}
+</style>
